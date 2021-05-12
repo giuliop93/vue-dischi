9d9bfb99571506615d73711de2ba1878adc8e5c3
Giulio_Pa <giuliop93@hotmail.it>
2021-05-12 18:21:25 +0200

    non funge

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const app = new Vue({
         genres: [],
         selectedGenre: ""
     },
-    // methods() {
-    //     onSelectChange(event) {
-    //         const select = event.currentTarget
-    //         select.value
-    //     }
-    // },
-    mounted() {
-        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
-            .then((resp) => {
-                console.log(resp)
-                this.albums = resp.data.response
-            })
-    }
-})
+    methods() {
+        this.dischi.forEach(disco => {
+            if (!this.generi.includes(disco.genre)) {
+                this.generi.push(disco.genre);
+            }
+        });
+        mounted() 
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+                .then((resp) => {
+                    console.log(resp)
+                    this.albums = resp.data.response
+                })
+        }
+    })
